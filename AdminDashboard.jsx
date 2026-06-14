a1f915cfc50ef9d4d81eb1fd30fa05c64d9c710b
Lingeswari <lingeswari2904@gmail.com>
2026-06-14 12:02:20 +0530

    Create AdminDashboard.jsx

---
+++ AdminDashboard.jsx
@@ -0,0 +1,24 @@
+function AdminDashboard() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+
+      <div>
+        <h3>Total Farmers</h3>
+        <p>120</p>
+      </div>
+
+      <div>
+        <h3>Total Scans</h3>
+        <p>340</p>
+      </div>
+
+      <div>
+        <h3>Most Common Disease</h3>
+        <p>Powdery Mildew</p>
+      </div>
+    </div>
+  );
+}
+
+export default AdminDashboard;
