cbfbbf6b9d33ba12edf08d37b23958dba341a08b
Lingeswari <lingeswari2904@gmail.com>
2026-06-14 12:03:43 +0530

    Create cors.php

---
+++ cors.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400'); // Cache for 1 day
+} else {
+    header("Access-Control-Allow-Origin: *");
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    }
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
+    http_response_code(200);
+    exit(0);
+}
+
+
+header("Content-Type: application/json; charset=UTF-8");
+?>
