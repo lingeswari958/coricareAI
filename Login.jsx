b30fe2dcd6367ed473d239fc7b0f616bd00c85cd
Lingeswari <lingeswari2904@gmail.com>
2026-06-14 12:01:36 +0530

    Create Login.jsx

---
+++ Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) =>
+          setEmail(e.target.value)
+        }
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) =>
+          setPassword(e.target.value)
+        }
+      />
+
+      <button type="submit">
+        Login
+      </button>
+    </form>
+  );
+}
+
+export default Login;
